9808af9a27f2c3e33751c55d298867cb9d7017bf
peazzoni1 <peazzoni@yahoo.com>
2016-01-06 09:38:35 -0500

    change background vid. add Masonry.js library to display portfolio

---
+++ app.js
@@ -0,0 +1,36 @@
+$(document).ready( function() {
+    $("#MyNavbar a").on('click', function (event) {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        });
+        
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+    });
+       
+    $('.grid-item').mouseover(function() {
+        $(this).find(".app-title").css('visibility','hidden');
+        $(this).find(".app-desc").css({'visibility':'visible'});
+        
+        $(this).children("a").children("img").css({'opacity': '.4'});
+    });
+    
+     $('.grid-item').mouseout(function() {
+        $(this).find(".app-desc").css('visibility', 'hidden');
+        $(this).find(".app-title").css({'visibility':'visible'});
+       $(this).children("a").children("img").css({'opacity': '1'});
+    });
+    
+    var vid = $("#video"); 
+    function setPlaySpeed() { 
+    vid.playbackRate = 4;
+    } 
+
+}); 
