3a6a58f496366cb69d0fe3cfdcdd840669a2f490
peazzoni1 <peazzoni@yahoo.com>
2016-01-11 15:21:43 -0500

    move images to own folder. Reorganize portfolio layout

--- ReadME.md
+++ ReadME.md
@@ -1,15 +1,6 @@
 # Personal Website
 
-### Welcome! 
+### Welcome!
 This is my personal website where I keep a showcase of apps and projects I’ve built or am currently working on. 
 
-The website its a single page app that includes an index.html and main.css. There’s an in-line script snippet for smooth scrolling. 
-
-Please feel free to leave comments or suggestions. 
-
-
-
-
-
-
-
+Please feel free to leave comments or suggestions.
--- app.js
+++ app.js
@@ -8,29 +8,29 @@
                 window.location.hash = hash;
             });
         });
-    $(function() { 
-     $('.grid').imagesLoaded(function() { 
+    $(function() {
+     $('.grid').imagesLoaded(function() {
           $('.grid').masonry({
             itemSelector: '.grid-item',
             columnWidth: '.grid-sizer',
             percentPosition: true,
-         
+
         });
-     }); 
+     });
     $('.grid-item').mouseover(function() {
         $(this).find(".app-title").css('visibility','hidden');
         $(this).find(".app-desc").css({'visibility':'visible'});
-        
+
         $(this).children("a").children("img").css({'opacity': '.4'});
     });
-    
+
      $('.grid-item').mouseout(function() {
         $(this).find(".app-desc").css('visibility', 'hidden');
         $(this).find(".app-title").css({'visibility':'visible'});
        $(this).children("a").children("img").css({'opacity': '1'});
         });
     });
-    var vid = $("#video"); 
-    function setPlaySpeed() { 
+    var vid = $("#video");
+    function setPlaySpeed() {
     vid.playbackRate = 4;
-    } 
+    }
