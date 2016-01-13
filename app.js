6d372c50ce94a29ff22d31402a364c56e450e872
peazzoni1 <peazzoni@yahoo.com>
2016-01-13 12:01:29 -0500

    updated portfolio and about sections

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+//Scroll to each section 
     $("#MyNavbar a").on('click', function (event) {
             event.preventDefault();
             var hash = this.hash;
@@ -8,6 +8,11 @@
                 window.location.hash = hash;
             });
         });
+
+//display "About" blurb 
+
+                   
+//load Masrony grid 
     $(function() {
      $('.grid').imagesLoaded(function() {
           $('.grid').masonry({
@@ -17,17 +22,21 @@
 
         });
      });
+        
+//hide app name and show description on mouseover
     $('.grid-item').mouseover(function() {
         $(this).find(".app-title").css('visibility','hidden');
+        $(this).find(".app-tag").css({'visibility':'hidden'});
         $(this).find(".app-desc").css({'visibility':'visible'});
-
         $(this).children("a").children("img").css({'opacity': '.4'});
     });
-
+        
+//hide description and show name again 
      $('.grid-item').mouseout(function() {
         $(this).find(".app-desc").css('visibility', 'hidden');
+        $(this).find(".app-tag").css({'visibility':'visible'});
         $(this).find(".app-title").css({'visibility':'visible'});
-       $(this).children("a").children("img").css({'opacity': '1'});
+        $(this).children("a").children("img").css({'opacity': '1'});
         });
     });
     var vid = $("#video");
